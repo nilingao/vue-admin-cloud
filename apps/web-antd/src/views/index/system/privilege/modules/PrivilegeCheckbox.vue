d9a310596771308b605c7eac371a45503250ca55
tzy <18789432816@126.com>
2025-09-21 22:15:18 +0800

    fix: 权限菜单部分提交，待优化

--- apps/web-antd/src/views/index/system/privilege/modules/PrivilegeCheckbox.vue
+++ apps/web-antd/src/views/index/system/privilege/modules/PrivilegeCheckbox.vue
@@ -1,18 +1,201 @@
+<script lang="ts" setup>
+import type { CheckboxGroupEntity } from '../model';
 
+import { ref, unref, watch } from 'vue';
 
+import { cloneDeep } from '@vben/utils';
+
+import { Button } from 'ant-design-vue';
+
+import MyCheckBox from './MyCheckBox.vue';
+
+const props = defineProps({
+  treeData: {
+    type: Array<CheckboxGroupEntity>,
+    default: () => [],
+  },
+  checkedList: {
+    type: Array<String>,
+    default: () => [],
+  },
+});
+const emit = defineEmits(['save']);
+const checkList = ref(props.checkedList);
+const dataTree = ref();
+const tree = ref(props.treeData);
+const last = ref();
+const onInit = () => {
+  // 给子级添加父级Key
+  // 深度克隆
+  const lastv = [] as string[];
+  const data = cloneDeep(props.treeData) as CheckboxGroupEntity[];
+  function addParentKey(data: CheckboxGroupEntity[], parentKey: any) {
+    data.forEach((ele) => {
+      const { children, v } = ele;
+      ele.parentId = parentKey;
+      ele.indeterminate = false;
+      ele.checked = false;
+      if (children) {
+        // 如果唯一标识不是code可以自行改变
+        addParentKey(children, ele.v);
+      } else {
+        lastv.push(v);
+        last.value = lastv;
+      }
+    });
+  }
+  addParentKey(data, undefined); // 一开始为undefined,根节点没有父级
+  dataTree.value = data;
+  tree.value = unref(dataTree);
+};
+// 切换权限监控
+watch(
+  () => props.checkedList,
+  (checkedList) => {
+    checkList.value = checkedList;
+    // 递归循环
+    checkList.value.forEach((obj: any) => {
+      loops(dataTree.value, true, obj);
+      tree.value = dataTree.value;
+    });
+  },
+);
+
+// 切换权限监控
+watch(
+  () => props.treeData,
+  (data) => {
+    tree.value = data;
+    onInit();
+  },
+);
+
+// 点击复选框触发
+const handleSubsetChange = async (val: any) => {
+  const { flag, v } = val;
+  await loops(dataTree.value, flag, v);
+  tree.value = dataTree.value;
+};
+
+// 递归循环
+const loops = (data: CheckboxGroupEntity[], flag: boolean, v: string) => {
+  data.forEach((item) => {
+    if (item.v === v) {
+      item.checked = flag;
+      item.indeterminate = false;
+      // 取消选中时数组中驱除元素
+      shuzulist(flag, item.v);
+      if (item.parentId) {
+        // 子级选中父级也选中
+        const childAndParent_Select = (code: string) => {
+          const parent = getItem(code) as CheckboxGroupEntity;
+          if (parent.children && parent.children.length > 0) {
+            const all = parent.children.every((item1) => {
+              return item1.checked === true && item1.indeterminate === false;
+            });
+            const some = parent.children.some((item1) => {
+              return item1.checked === true || item1.indeterminate === true;
+            });
+            if (all) {
+              parent.indeterminate = false;
+              parent.checked = true;
+              // 取消选中时数组中驱除元素
+              shuzulist(true, parent.v);
+            } else if (some) {
+              parent.indeterminate = true;
+              parent.checked = false;
+              shuzulist(false, parent.v);
+            } else {
+              parent.indeterminate = false;
+              parent.checked = false;
+              shuzulist(false, parent.v);
+            }
+          }
+          if (parent.parentId) {
+            childAndParent_Select(parent.parentId);
+          }
+        };
+        childAndParent_Select(item.parentId);
+      }
+      if (item.children && item.children.length > 0) {
+        // 父亲选中，子级全选中，实现全选反选
+        const parentAndChild_Select = (
+          data: CheckboxGroupEntity[],
+          flag: boolean,
+        ) => {
+          data.forEach((item1) => {
+            item1.checked = flag;
+            item1.indeterminate = false;
+            shuzulist(flag, item1.v);
+            if (item1.children && item1.children.length > 0) {
+              parentAndChild_Select(item1.children, flag);
+            }
+          });
+        };
+        parentAndChild_Select(item.children, flag);
+      }
+    }
+
+    if (item.children && item.children.length > 0) {
+      loops(item.children, flag, v);
+    }
+  });
+};
+
+// 根据code(唯一标识)找到其值
+const getItem = (code: string) => {
+  let value: CheckboxGroupEntity | undefined;
+  const loops = (data: CheckboxGroupEntity[], code: string) => {
+    data.forEach((item) => {
+      if (item.v === code) {
+        value = item;
+      }
+      if (item.children && item.children.length > 0) {
+        loops(item.children, code);
+      }
+    });
+  };
+  loops(unref(dataTree), code);
+  return value;
+};
+
+// 数组新增元素删除元素去重
+const shuzulist = (flag: boolean, v: string) => {
+  if (flag) {
+    const cheList = [...unref(checkList)];
+    cheList.push(v);
+    const list = [...new Set(unref(cheList))];
+    checkList.value = list;
+  } else {
+    const cheList = [...new Set(unref(checkList))];
+    // 元素是否存在数组
+    if (!cheList.includes(v)) {
+      cheList.splice(cheList.indexOf(v), 1);
+      checkList.value = cheList;
+    }
+  }
+};
+
+// 抛出选中数组方法
+const handleSave = () => {
+  // 只要最下级元素
+  const resultArray = unref(checkList).filter((item) =>
+    unref(last).includes(item),
+  );
+  emit('save', resultArray);
+};
+
+defineExpose({ onInit });
+</script>
 <template>
   <div class="m-4 flex flex-col bg-white">
     <div class="justify-end">
-      <a-button
-        class="float-right mr-4 mt-4"
-        type="primary"
-        @click="handleSave"
-      >
+      <Button class="float-right mr-4 mt-4" type="primary" @click="handleSave">
         保存
-      </a-button>
+      </Button>
     </div>
     <div class="grow overflow-hidden">
-      <MyCheckBox :treeData="tree" @subset="handleSubsetChange" />
+      <MyCheckBox :tree-data="tree" @subset="handleSubsetChange" />
     </div>
   </div>
 </template>
